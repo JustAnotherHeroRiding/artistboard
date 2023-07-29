dd9c7ca5810e44ba97b59a5f0889339b2c625329
Kristijan Kocev <kristijankocev1234@gmail.com>
2023-07-29 14:27:37 +0200

    images

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,22 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        reenie: ["Reenie Beanie", "cursive"],
+        Roboto: "Roboto",
+      },
+      colors: {
+        brainster: {
+          100: "#FCEBD5",
+          200: '#A16A5E',
+        }
+      },
+      boxShadow: {
+          figma: '0px 2px 2px 0px rgba(0, 0, 0, 0.25);'
+
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
